8fc73e90222eacc2022ca33351dbcaa508c7022b
Evette Cheung <FrancFranc@gmx.com>
2017-04-13 10:59:44 -0700

    Assignment modified.

--- app.js
+++ app.js
@@ -32,74 +32,77 @@ SalesPerLocation.prototype.totalCookiesFunc =  function() {
                                                };
 
 
-SalesPerLocation.prototype.saleTable =  function() {
-                                          var table =  document.createElement('table');
+SalesPerLocation.prototype.saleTableRow1 =  function() {
+                                              var table =  document.createElement('table');
 
-                                          var tRow1, tData1, tDataEmpty, tDataDailyTotalString ;
+                                              var tRow1, tData1, tDataEmpty, tDataDailyTotalString ;
 
-                                            tRow1 = document.createElement('tr');
-                                            tDataEmpty = document.createElement ('td');
-                                            tRow1.appendChild(tDataEmpty);
+                                              tRow1 = document.createElement('tr');
+                                              tDataEmpty = document.createElement ('td');
+                                              tRow1.appendChild(tDataEmpty);
 
-                                            for (var i=0; i<this.businessHours.length; i++) {
+                                              for (var i=0; i<this.businessHours.length; i++) {
                                                 tData1 = document.createElement('td');
                                                 tData1.textContent = this.businessHours[i];
                                                 tRow1.appendChild(tData1);
                                               }
 
-                                            tDataDailyTotalString = document.createElement('td');
-                                            tRow1.appendChild(tDataDailyTotalString);
-                                            tDataDailyTotalString.textContent = "Daily Location Total";
-                                            table.appendChild(tRow1);
+                                              tDataDailyTotalString = document.createElement('td');
+                                              tRow1.appendChild(tDataDailyTotalString);
+                                              tDataDailyTotalString.textContent = "Daily Location Total";
+                                              table.appendChild(tRow1);
 
+                                              return table;
+                                            }
 
-                                          for (var i=0; i<6; i++) {
+SalesPerLocation.prototype.saleTableRow2 = function () {
+                                             var tRow2, tLocations, tData2, tDataDailyTotal;
 
-                                            var tRows, tDatum, tLocations;
+                                             tRow2 = document.createElement('tr');
+                                             table.appendChild(tRow2);
 
-                                              tRows = document.createElement('tr');
-                                              tLocations = document.createElement ('td');
-                                              tLocations.textContent = this.Location;
-                                              tRows.appendChild(tLocations);
+                                             tLocations = document.createElement('td');
+                                             tLocations.textContent = location;
+                                             tRow2.appendChild(tLocations);
 
-                                              for (var i=0; i<this.simulatedCookiesArray.length; i++) {
-                                                  tDatum = document.createElement('td');
-                                                  tRows.appendChild(tDatum);
-                                                  tDatum.textContent = this.simulatedCookiesArray[i];
+                                             for (var i=0; i<this.businessHours.lenghth; i++) {
+                                               tData2 = document.createElement('td');
+                                               tData2.textContent = this.simulatedCookiesArray[i];
+                                               tRow2.appendChild(tData2);
+                                             }
 
-                                              }
+                                             tDataDailyTotal = document.craeteElement('td');
+                                             tRow2.appendChild(tDataDailyTotal);
+                                             tDataDailyTotal.textContent = this.totalCookiesFunc();
+                                             tRow2.appendChild(tDataDailyTotal);
+                                           }
 
-                                              tDataDailyTotal = document.createElement('td');
-                                              tRows.appendChild(tDataDailyTotal);
-                                              tDataDailyTotal.textContent = this.totalCookiesFunc();
-
-                                              table.appendChild(tRow2);
-                                          }
-
-                                          return table;
-                                        }
 
 var pike = new SalesPerLocation (23, 65, 6.3, "1st & Pike");
-var pikeTable = pike.saleTable();
-var pikePosition = document.getElementById('dailysales-table');
-pikePosition.appendChild(pikeTable);
+var pikeHTMLPosition = document.getElementById('dailysales-table');
+var pikeTRow1 = pike.saleTableRow1();
+pikeHTMLPosition.appendChild(pikeTRow1);
+var pikeTRow2 = pike.saleTableRow2();
+pikeHTMLPosition.appendChild(pikeTRow2);
 
-var SeaTac = new SalesPerLocation (3, 24, 1.2, "SeaTac Airport");
-var seaTacTable = seaTac.saleTable();
-var seaTacPosition = document.getElementById('dailysales-table');
-seaTacPosition.appendChild(seaTacTable);
 
-var seattleCenter= new SalesPerLocation (11, 38, 3.7, "Seattle Center");
-var seattleCenterTable = seattleCenter.saleTable();
-var seattleCenterPosition = document.getElementById('dailysales-table');
-seattleCenterPosition.appendChild(pikeTable);
-
-var capitolHill = new SalesPerLocation (20, 38, 2.3, "Capitol Hill");
-var capitolHillTable = capitolHill.saleTable();
-var capitolHillPosition = document.getElementById('dailysales-table');
-capitolHillPosition.appendChild(capitolHillTable);
-
-var alki = new SalesPerLocation (2, 16, 4.6, "Alki");
-var alkiTable = alki.saleTable();
-var alkiPosition = document.getElementById('dailysales-table');
-alkiPosition.appendChild(alkiTable);
+//
+// var SeaTac = new SalesPerLocation (3, 24, 1.2, "SeaTac Airport");
+// var seaTacTable = seaTac.saleTable();
+// var seaTacPosition = document.getElementById('dailysales-table');
+// seaTacPosition.appendChild(seaTacTable);
+//
+// var seattleCenter= new SalesPerLocation (11, 38, 3.7, "Seattle Center");
+// var seattleCenterTable = seattleCenter.saleTable();
+// var seattleCenterPosition = document.getElementById('dailysales-table');
+// seattleCenterPosition.appendChild(pikeTable);
+//
+// var capitolHill = new SalesPerLocation (20, 38, 2.3, "Capitol Hill");
+// var capitolHillTable = capitolHill.saleTable();
+// var capitolHillPosition = document.getElementById('dailysales-table');
+// capitolHillPosition.appendChild(capitolHillTable);
+//
+// var alki = new SalesPerLocation (2, 16, 4.6, "Alki");
+// var alkiTable = alki.saleTable();
+// var alkiPosition = document.getElementById('dailysales-table');
+// alkiPosition.appendChild(alkiTable);
